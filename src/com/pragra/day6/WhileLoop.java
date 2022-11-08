0717be22e5eeb9344527467513bc80487f3f1542
Daljeetkaur320 <116773878+Daljeetkaur320@users.noreply.github.com>
2022-11-07 22:07:29 -0500

    Update WhileLoop.java

--- src/com/pragra/day6/WhileLoop.java
+++ src/com/pragra/day6/WhileLoop.java
@@ -8,7 +8,7 @@ public class WhileLoop {
         System.out.println("Hello World");
         System.out.println("Hello World");
         System.out.println("Hello World");
-
+      System.out.println("Hello");
     }
 
 }
