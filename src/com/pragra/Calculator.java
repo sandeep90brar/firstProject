3c74dbb0b138015ba6d80549a54d26c1c2222c73
Daljeetkaur320 <116773878+Daljeetkaur320@users.noreply.github.com>
2022-11-07 21:13:19 -0500

    Update Calculator.java

--- src/com/pragra/Calculator.java
+++ src/com/pragra/Calculator.java
@@ -2,23 +2,20 @@ package com.pragra;
 
 public class Calculator {
     public static void main(String[] args) {
-        int sumResult = sum(9, 2);
+        
         int diffResult = difference(9, 5);
         int multiplyResult = multiply(6, 4);
         double divideResult = divide(5, 3);
 
 
-        System.out.println("The sum:" + sumResult);
+       
         System.out.println("The difference:" + diffResult);
         System.out.println(" The multiply:" + multiplyResult);
         System.out.println(" The divide:" + divideResult);
 
     }
 
-    public static int sum(int x, int y) {
-
-        return x + y;
-    }
+   
 
     public static int difference(int y, int x) {
         return y - x;
