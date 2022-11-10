6e382cf2ddf9bcb9a4b79d2417edfe462d50f437
HarshaKarkera <harshakarkera97@gmail.com>
2022-11-09 21:20:16 -0500

    Hk made chaged to day9

--- src/com/pragra/day9/StringsDemo.java
+++ src/com/pragra/day9/StringsDemo.java
@@ -1,4 +1,11 @@
 package com.pragra.day9;
 
-public class StringsDemo {
+public class StringsDemo
+{
+    public static void main(String[] args)
+    {
+        String st="Java Development";
+
+
+    }
 }
