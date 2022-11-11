d35e2768825d7620947515519271960a68a26c2d
Karansharma98 <sharmakaran9382@gmail.com>
2022-11-10 20:24:12 -0500

    karan changes

--- src/com/pragra/day9/StringsDemo.java
+++ src/com/pragra/day9/StringsDemo.java
@@ -1,11 +1,14 @@
 package com.pragra.day9;
 
+import java.sql.SQLOutput;
+
 public class StringsDemo
 {
     public static void main(String[] args)
     {
         String st="Java Development";
         System.out.println("Karan Changes");
+        System.out.println("git practice");
 
     }
 }
